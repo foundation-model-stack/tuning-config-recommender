dad0514086da34e9110b07ff04a5e86194d8a528
SRIKUMAR VENUGOPAL <srikumarv@ie.ibm.com>
2025-12-09 09:23:19 +0000

    chore: linting using ruff
    Signed-off-by: SRIKUMAR VENUGOPAL <srikumarv@ie.ibm.com>

--- src/recommender/actions/compute.py
+++ src/recommender/actions/compute.py
@@ -4,15 +4,15 @@ from loguru import logger
 
 from .actions import IR, Action, Comment, PatchLevel, PatchType
 
+
 class ApplyComputeConfig(Action):
 
     def _infer_model_name(self, m:str) -> str:
         """
-        Docstring for _infer_model_name
-        
+        Method to infer model name from model_name_or_path parameter in the IR
         :param m: model_name_or_path parameter value from the IR
         :type m: str
-        :return: the model name to pass to the min gpu recommender. 
+        :return: the model name to pass to the min gpu recommender.
         This has been extracted from the IR and mapped to a model name in the Min GPU recommender database. 
         The mapping will return the input if no match is found.
         :rtype: str
@@ -44,28 +44,30 @@ class ApplyComputeConfig(Action):
 
         logger.debug(f"IR has the configuration workers:{num_nodes} gpus:{num_gpus_per_node}")
         # invoke min_gpu_recommender
-        # 
+        #
         recommender = MinGpuRecommenderCaller()
 
         r_model_name = ir.train_config.get("model_name_or_path", None)
         if not r_model_name:
             raise Exception(f"model name was not populated in the representation {ir}")
-        
+
         #Find a valid mapping for the model name
         r_model_name = self._infer_model_name(r_model_name)
 
         #Check if tuning method is enabled, if not go for full
-        r_method = ir.train_config.get("tuning_strategy", "full") 
+        r_method = ir.train_config.get("tuning_strategy", "full")
 
         #set GPU model - for now we assume it's for Vela
+        #TODO: obtain some information from the llm.build space to determine which
+        #cluster, GPU, etc. are being targeted
         r_gpu = "NVIDIA-A100-SXM4-80GB"
 
         #set batch size - this is tricky because we get the per-device batch size from IR
-        #while the model uses batch size across gpus. 
-        #Current solution - test every #GPU in 1,2,4,8 and pick the smallest one that we get 
+        #while the recommender model uses batch size across gpus.
+        #Current solution - test every #GPU in 1,2,4,8 and pick the smallest one that we get
         #recommendation for
-        r_max_seq_length = ir.train_config.get("max_seq_length",2048) 
-        r_batch_size = ir.train_config.get("per_device_batch_size", 8)
+        r_max_seq_length = ir.train_config.get("max_seq_length", 2048)
+        r_batch_size = ir.train_config.get("per_device_batch_size", 1)
 
         for r_num_gpu in [1,2,4,8]:
             configuration = {
@@ -74,6 +76,7 @@ class ApplyComputeConfig(Action):
                 "gpu_model": r_gpu, #Assume it's Vela
                 "tokens_per_sample": r_max_seq_length,
                 "batch_size": r_batch_size*r_num_gpu,
+                "gpus_per_worker": r_num_gpu,
                 "model_version": "2.0.0"
             }
             logger.debug(f"Sending this configuration to min gpu recommender: {configuration}")
@@ -83,7 +86,7 @@ class ApplyComputeConfig(Action):
                 continue
             else:
                 num_gpus_per_node = res["gpus_per_worker"]
-                num_nodes = res["workers"] 
+                num_nodes = res["workers"]
                 logger.debug(f"recommender returned configuration workers:{num_nodes} gpus:{num_gpus_per_node}")
                 break
 
