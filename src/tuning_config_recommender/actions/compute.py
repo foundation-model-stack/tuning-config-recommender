b886275240dc168e5f91f3b66cd920cbb85a96ce
SRIKUMAR VENUGOPAL <srikumarv@ie.ibm.com>
2026-02-06 10:23:10 +0000

    remove model interface code from compute action
    Signed-off-by: SRIKUMAR VENUGOPAL <srikumarv@ie.ibm.com>

--- src/tuning_config_recommender/actions/compute.py
+++ src/tuning_config_recommender/actions/compute.py
@@ -3,7 +3,6 @@ from loguru import logger
 from .actions import IR, Action, Comment, PatchLevel, PatchType
 
 try:
-    from autoconf.utils.config_mapper import map_valid_model_name
     from fm_training_estimator.regressor.min_gpu.recommender import (
         MinGpuRecommenderCaller,
     )
@@ -28,32 +27,6 @@ class ApplyComputeConfig(Action):
     def heuristic_skip(self, ir):
         return skip_autoconf
 
-    def _infer_model_name(self, m: str) -> str:
-        """
-        Method to infer model name from model_name_or_path parameter in the IR
-        :param m: model_name_or_path parameter value from the IR
-        :type m: str
-        :return: the model name to pass to the min gpu recommender.
-        This has been extracted from the IR and mapped to a model name in the Min GPU recommender database.
-        The mapping will return the input if no match is found.
-        :rtype: str
-        """
-        logger.debug(f"Model path received: {m}")
-        # When a unique tag is given, the model path will contain an additional path component at the end
-        # Thus we will start from the last path component and work backwards for atleast one more mapping
-        for _ in range(2):
-            components = m.rpartition("/")
-            logger.debug(f"Model path split into: {components}")
-            r_name = map_valid_model_name(components[2])
-            if r_name == components[2]:
-                m = components[0]
-                continue
-            else:
-                break
-        logger.debug(f"Stopped at result: {r_name}")
-
-        return r_name
-
     def apply(self, ir: IR, actions_meta: list[str]) -> IR:
         if "skip_estimator" in actions_meta:
             self.skip = True
@@ -63,6 +36,18 @@ class ApplyComputeConfig(Action):
             return
         # TODO: fast kernels are not supported for some optimizer classes
         # we should either edit this or skip this optimization
+
+        # Test for presence of required configs
+        if not ir.compute_config:
+            logger.warning("compute_config is not present in IR, skipping compute action")
+            self.skip = True
+            return
+
+        if not ir.tuning_config:
+            logger.warning("tuning_config is not present in IR, skipping compute action")
+            self.skip = True
+            return
+
         num_nodes = ir.compute_config.get("num_nodes", 1)
         num_gpus_per_node = ir.compute_config.get("num_gpus_per_node", 8)
 
@@ -75,9 +60,6 @@ class ApplyComputeConfig(Action):
         if not r_model_name:
             raise Exception(f"model name was not populated in the representation {ir}")
 
-        # Find a valid mapping for the model name
-        r_model_name = self._infer_model_name(r_model_name)
-
         # Check if tuning method is enabled, if not go for full
         r_method = ir.tuning_config.get("tuning_strategy", "full")
 
@@ -86,39 +68,37 @@ class ApplyComputeConfig(Action):
         # cluster, GPU, etc. are being targeted
         r_gpu = "NVIDIA-A100-SXM4-80GB"
 
-        # set batch size - this is tricky because we get the per-device batch size from IR
-        # while the recommender model uses batch size across gpus.
-        # Current solution - test every #GPU in 1,2,4,8 and pick the smallest one that we get
-        # recommendation for
         r_max_seq_length = ir.tuning_config.get("max_seq_length", 2048)
-        r_batch_size = ir.tuning_config.get("per_device_batch_size", 1)
-
-        for r_num_gpu in [1, 2, 4, 8]:
-            configuration = {
+        r_batch_size = ir.tuning_config.get("per_device_train_batch_size", 1)
+        configuration = {
                 "model_name": r_model_name,
                 "method": r_method,
-                "gpu_model": r_gpu,  # Assume it's Vela
+                "gpu_model": r_gpu, #Assume it's Vela
                 "tokens_per_sample": r_max_seq_length,
-                "batch_size": r_batch_size * r_num_gpu,
-                "gpus_per_worker": r_num_gpu,
-                "model_version": "2.0.0",
-            }
-            logger.debug(
-                f"Sending this configuration to min gpu recommender: {configuration}"
-            )
-            res = self._recommender.run(configuration, "min_gpu")
-            if res["gpus_per_worker"] == -1:
-                logger.debug(
-                    f"Recommender was not able to issue recommender for {configuration}"
-                )
-                continue
-            else:
+                "per_device_train_batch_size": r_batch_size,
+        }
+
+        logger.debug(f"Sending this configuration to min gpu recommender: {configuration}")
+
+        res = self._recommender.run(configuration, "avoid_oom")
+        if res["gpus_per_worker"] == -1:
+            logger.debug(f"Recommender was not able to issue recommender for {configuration}")
+        else:
+            logger.debug(f"Recommender recommends - {res['workers']} {res['gpus_per_worker']}; original {num_nodes} {num_gpus_per_node}")
+            total_gpus_orig = num_gpus_per_node * num_nodes
+            total_gpus_rec = res["gpus_per_worker"] * res["workers"]
+            #Since we are trying to avoid OOM, we only replace if the recommendation is higher than original request as this means that
+            #the number of gpus was too low to avoid OOM
+            if total_gpus_rec > total_gpus_orig:
+                logger.debug("Replacing original compute config with recommender's suggestion")
                 num_gpus_per_node = res["gpus_per_worker"]
                 num_nodes = res["workers"]
-                logger.debug(
-                    f"recommender returned configuration workers:{num_nodes} gpus:{num_gpus_per_node}"
-                )
-                break
+            else:
+                logger.debug("Recommender's suggestion is lower than original request, so not replacing")
+        
+        #Update fast_moe section 
+        #Needs confirmation from the devs
+        
 
         return_ir = IR(
             compute_config={
